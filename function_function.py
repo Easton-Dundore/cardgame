58ac5de3ed80903be8c07d8e2503b1aca9c277fc
Easton-Dundore <78653755+Easton-Dundore@users.noreply.github.com>
2021-05-24 18:18:05 -0400

    Add files via upload

--- function_function.py
+++ function_function.py
@@ -41,7 +41,7 @@ class Game:
 
     def deal(self):
         for player in self.players:
-            for i in range(2):
+            for i in range(3):
                 player.give_card(self.draw_card())
 
     def draw_card(self):
@@ -71,75 +71,106 @@ class Player:
         return 'Player: {} - [{}]'.format(self.name, ','.join([str(x) for x in self.cards]))
 
 
-
-def total_score(list1, list2):
+    #def compare_hands(list1, list2):
+    # compare the contents of two lists
+    # at the end, return which hand won
+def total_score(list1, list2, list3):
     v11 = list1[0].value
     v12 = list1[1].value
     v21 = list2[0].value
     v22 = list2[1].value
+    v31 = list3[0].value
+    v32 = list3[1].value
     three = v11 + v12
     four = v21 + v22
-    if v11 == v12 and v21 == v22:
-        if three > four:
-            print ('Player one has a total score of ' + (str(three)) + ',' + 'and player two has ' + (str(four)) + ' player one wins!!!')
-        elif four > three:
-            print ('Player two has a total score of ' + (str(four)) + ',' + 'and player one has ' + (str(three)) + ' player two wins!!!')
+    five = v31 + v32
     
-    elif v11 == v12:
-        print ('Player one has equal cards, they win!')
-    elif v21 == v22:
-        print ('Player two has equal cards, they win!')
-
-    else:
-        if three > four:
-            print ('Player one has a total score of ' + (str(three)) + ',' + 'and player two has ' + (str(four)) + ' player one wins!!!')
-        elif four > three:
-            print ('Player two has a total score of ' + (str(four)) + ',' + 'and player one has ' + (str(three)) + ' player two wins!!!')
-        else:
-            print ('Both players have the same total number, it is a tie!!!')
-
-    
-
-    #def compare_hands(list1, list2):
-    # compare the contents of two lists
-    # at the end, return which hand won
 
 def compare_two_lists(list1, list2):
     n1 = len(list1)
     n2 = len(list2)
     v11 = list1[0].value
     v12 = list1[1].value
+    v13 = list1[2].value
     v21 = list2[0].value
-    v22 = list2[0].value
+    v22 = list2[1].value
+    v23 = list2[2].value
+   
+    hv1 = sum(v11, v12, v13)
+    hv2 = sum(v21, v22, v23)
+
     
-    hv1 = max(v11, v12)
-    hv2 = max(v21, v22)
-    print('highest card in list 1 has value ' + str(hv1))
-    print('The highest card in list 2 has value ' + str(hv2))
-    retlist = []
-    if n1 > n2:
-        print('list 1 has more elements (' + str(n1) + ') than list 2 (' + str(n2) + ')')
-        retlist = list1
-    elif n2 > n1:
-        print('list 2 has more elements (' + str(n2) + ') than list 1 (' + str(n1) + ')')
-        retlist = list2
+
+def check_pair(hand):
+    v0 = hand[0].value
+    v1 = hand[1].value
+    v2 = hand[2].value
+    if v0 == v1 or v0 == v2 or v1 == v2:
+        has_pair = True
+        pair_val = v0
     else:
-        print('list 1 has the same number of elements (' + str(n1) + ') as list 2')
-        retlist = list1
-    return retlist
+        has_pair = False
+        pair_val = 0
+    return [has_pair, pair_val]
+
+def check_high_card(hand):
+    v0 = hand[0].value
+    v1 = hand[1].value
+    v2 = hand[2].value
+    # return max(v0, v1)  # returns high card
+    return v0 + v1 + v2       # returns sum of card values
+name1 = 'Tom' 
+name2 = 'Harry'
+ 
+deck_of_cards = Deck()
+player_one = Player(name1)
+player_two = Player(name2)
 '''
-def check_one_pairs(hand):
-    values = [i[0] for i in hand]
-    value_counts = defaultdict(lambda:0)
-    for v in values:
-        value_counts[v]+=1
-    if 2 in value_counts.values():
-        return True
+test_card11 = Card("Hearts", 2)
+test_card12 = Card("Clubs", 4)
+player_one.give_card(test_card11)
+player_one.give_card(test_card12)
+test_card21 = Card("Hearts", 2)
+test_card22 = Card("Clubs", 4)
+player_two.give_card(test_card21)
+player_two.give_card(test_card22)
+'''
+new_game = Game([player_one, player_two], deck_of_cards)
+new_game.deal()
+print(player_one, player_two)
+hand_one = player_one.return_cards()
+hand_two = player_two.return_cards()
+
+
+hand_one_score = check_pair(hand_one)
+hand_two_score = check_pair(hand_two)
+hand_one_high = check_high_card(hand_one)
+hand_two_high = check_high_card(hand_two)
+
+if hand_one_score[0] == True and hand_two_score[0] == True:
+    if hand_one_score[1] > hand_two_score[1]:
+        print(name1 + ' has a pair of ' + str(hand_one_score[1]) + 's and wins!')
+    elif hand_one_score[1] < hand_two_score[1]:
+        print(name2 + ' has a pair of ' + str(hand_two_score[1]) + 's and wins!')
     else:
-        return False
+        print(name1 + ' and ' + name2 + ' both have pairs of ' + str(hand_one_score[0]) + ': it is a draw!')
+
+else:
+    if hand_one_score[0] == True:
+        print(name1 + ' has a pair of ' + str(hand_one_score[1]) + 's and wins!')
+    elif hand_two_score[0] == True:
+        print(name2 + ' has a pair of ' + str(hand_two_score[1]) + 's and wins!')
+    else:
+        if hand_one_high > hand_two_high:
+            print(name1 + ' has a higher total of ' + str(hand_one_high) + ' and wins!')
+        elif hand_one_high < hand_two_high:
+            print(name2 + ' has a higher total of ' + str(hand_two_high) + ' and wins!')
+        else:
+            print(name1 + ' and ' + name2 + ' have the same total of ' + str(hand_one_high) + ': it is a draw!')
+
 '''
-name1 = input('Enter the name of player one: ')
-name2 = input('Enter the name of player two: ')
+#name1 = input('Enter the name of player one: ')
+#name2 = input('Enter the name of player two: ')
 deck_of_cards = Deck()
 player_one = Player(name1)
 player_two = Player(name2)
@@ -159,3 +190,4 @@ hand_two = player_two.return_cards()
 #new_game.restart_game()
 #print(player_one, player_two)
 #print(new_game.deck)
+'''
